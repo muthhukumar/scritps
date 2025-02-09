ea620c3af714332bc98c8484c2478822a09044f4
Muthukumar <muthukumarperiyasamy.mk@gmail.com>
2025-02-09 12:31:20 +0530

    Update auto-scroll.user.js

--- auto-scroll.user.js
+++ auto-scroll.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
-// @name         Auto Scroll Every 5s
+// @name         Auto Scroll Every 35 seconds
 // @namespace    https://github.com/muthhukumar/scripts
 // @version      1.0
 // @description  Smoothly scrolls 35% of the screen every 35000 seconds
-// @author       Your Name
+// @author       Muthukumar P
 // @match        *://*/*
 // @grant        none
 // @downloadURL  https://raw.githubusercontent.com/muthhukumar/scripts/main/auto-scroll.user.js
@@ -12,7 +12,7 @@
 
 (function scrollPage() {
     let scrollInterval = setInterval(() => {
-        let scrollAmount = window.innerHeight * 0.35; /
+        let scrollAmount = window.innerHeight * 0.35; 
         if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
             clearInterval(scrollInterval);
         } else {
