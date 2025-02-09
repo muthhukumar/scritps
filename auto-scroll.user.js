270becb54330e37e881a9583726a68d3881fcec9
Muthukumar <muthukumarperiyasamy.mk@gmail.com>
2025-02-09 14:35:30 +0530

    Update auto-scroll.user.js

--- auto-scroll.user.js
+++ auto-scroll.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Auto Scroll Every 30 seconds
 // @namespace    https://github.com/muthhukumar/scripts
-// @version      1.4
+// @version      1.5
 // @description  Smoothly scrolls 35% of the screen every 35000 seconds
 // @author       Muthukumar P
 // @match        *://*/*
@@ -11,12 +11,27 @@
 // ==/UserScript==
 
 (function scrollPage() {
-    let scrollInterval = setInterval(() => {
-        let scrollAmount = window.innerHeight * 0.48;
-        if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
-            clearInterval(scrollInterval);
-        } else {
-            window.scrollBy({ top: scrollAmount, left: 0, behavior: "smooth" });
-        }
-    }, 45000);
+    let scrollPercentage = 0.48;
+    let userScrollFactor = 0.3;
+    let scrollIntervalTime = 45000;
+    let userTimeFactor = 0.5;
+
+    function startScrolling(scrollAmount, interval) {
+        let intervalId = setInterval(() => {
+            if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+                clearInterval(intervalId);
+            } else {
+                window.scrollBy({ top: window.innerHeight * scrollAmount, left: 0, behavior: "smooth" });
+            }
+        }, interval);
+    }
+
+    let actualScrollAmount = scrollPercentage;
+    let actualScrollTime = scrollIntervalTime;
+
+    let userScrollAmount = actualScrollAmount * userScrollFactor;
+    let userScrollTime = actualScrollTime * userTimeFactor;
+
+    startScrolling(actualScrollAmount, actualScrollTime);
+    startScrolling(userScrollAmount, userScrollTime);
 })();
